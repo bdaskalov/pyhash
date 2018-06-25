a9253f1e98a949fc74c829ef5e54fc4bc19aeb03
Boris Daskalov <boris@hyperscience.com>
2018-06-25 17:34:39 -0400

    some improvements.

--- pyhash.cc
+++ pyhash.cc
@@ -2,23 +2,8 @@
 #include <map>
 #include <cstdio>
 // #include <sparsehash/dense_hash_map>
-// #include <sparsehash/sparse_hash_map>
+#include <sparsehash/sparse_hash_map>
 #include <tr1/unordered_map>
-
-static PyObject *
-greet_name(PyObject *self, PyObject *args)
-{
-    const char *name;
-
-    if (!PyArg_ParseTuple(args, "s", &name))
-    {
-        return NULL;
-    }
-
-    printf("Hello %s!\n", name);
-
-    Py_RETURN_NONE;
-}
 
 class MapIterator {
 public:
@@ -32,7 +17,7 @@ public:
     virtual ~Map() {};
     virtual ssize_t size() = 0;
     virtual PyObject* get(PyObject* key) = 0;
-    virtual void put(PyObject *key, PyObject *val) = 0;
+    virtual bool put(PyObject *key, PyObject *val) = 0;
     virtual MapIterator* makeIterator() = 0;
 };
 
@@ -45,12 +30,23 @@ long long from_pyobj(PyObject* val) {
     return PyLong_AsLongLong(val);
 }
 
+template <>
+int from_pyobj(PyObject* val) {
+    assert(val != NULL);
+    return PyLong_AsLong(val);
+}
+
 template <typename T>
 PyObject* to_pyobj(T val);
 
 template <>
 PyObject* to_pyobj(long long val) {
     return PyLong_FromLongLong(val);
+}
+
+template <>
+PyObject* to_pyobj(int val) {
+    return PyLong_FromLong(val);
 }
 
 
@@ -84,13 +80,16 @@ class MapImpl : public Map {
 public:
     // typedef std::map<K, V> MapType;
     // typedef google::dense_hash_map<K, V> MapType;
-    // typedef google::sparse_hash_map<K, V> MapType;
-    typedef std::tr1::unordered_map<K, V> MapType;
+    typedef google::sparse_hash_map<K, V> MapType;
+    // typedef std::tr1::unordered_map<K, V> MapType;
 
     typedef typename MapType::iterator IteratorType;
 
     MapImpl() {
+        // For dense_hash_map
         // _map.set_empty_key((K)-1);
+        // For sparse_hash_map
+        _map.set_deleted_key(-1);
     }
 
     virtual ssize_t size() {
@@ -106,15 +105,16 @@ public:
         return to_pyobj<V>(it->second);
     }
 
-    virtual void put(PyObject *key, PyObject *val) {
+    virtual bool put(PyObject *key, PyObject *val) {
         K intkey = from_pyobj<K>(key);
         if (val) {
             V intval = from_pyobj<V>(val);
             _map[intkey] = intval;
+            return true;
         } else {
-            _map.erase(intkey);
+            bool exists = (1 == _map.erase(intkey));
+            return exists;
         }
-        // TODO: raise KeyError on non-existing key.
     }
 
     virtual MapIterator* makeIterator() {
@@ -130,18 +130,17 @@ class MapImpl<K, PyObject*> : public Map {
 public:
     // typedef std::map<K, PyObject*> MapType;
     // typedef google::dense_hash_map<K, PyObject*> MapType;
-    // typedef google::sparse_hash_map<K, PyObject*> MapType;
-    typedef std::tr1::unordered_map<K, PyObject*> MapType;
+    typedef google::sparse_hash_map<K, PyObject*> MapType;
+    // typedef std::tr1::unordered_map<K, PyObject*> MapType;
     typedef typename MapType::iterator IteratorType;
 
     MapImpl() {
         // _map.set_empty_key((K)-1);
-        // _map.set_deleted_key(-1);
+        _map.set_deleted_key(-1);
     }
 
     virtual ~MapImpl() {
         for(IteratorType it=_map.begin(); it != _map.end(); ++it) {
-            // std::printf("%lld %lld\n", it->first, it->second->ob_refcnt);
             Py_DECREF(it->second);
         }
     }
@@ -161,7 +160,7 @@ public:
         return it->second;
     }
 
-    virtual void put(PyObject *key, PyObject *val) {
+    virtual bool put(PyObject *key, PyObject *val) {
         K intkey = from_pyobj<K>(key);
         IteratorType it = _map.find(intkey);
         if (it != _map.end()) {
@@ -169,16 +168,18 @@ public:
             if (val) {
                 Py_INCREF(val);
                 it->second = val;
+                return true;
             } else {
                 _map.erase(intkey);
+                return true;
             }
         } else {
             if (val) {
                 Py_INCREF(val);
                 _map[intkey] = val;
+                return true;
             } else {
-                // Double delete. We need to raise key error here to be compatible.
-                // PyErr_SetObject(PyExc_KeyError, key)
+                return false;
             }
         }
     }
@@ -207,6 +208,10 @@ IIMap_init(pyhash_IIMapObject *self, PyObject *args, PyObject *kwds)
         self->map = new MapImpl<long long, long long>();
     } else if (keytype == 'L' && valtype == 'O') {
         self->map = new MapImpl<long long, PyObject*>();
+    } else if (keytype == 'I' && valtype == 'I') {
+        self->map = new MapImpl<int, int>();
+    } else if (keytype == 'I' && valtype == 'O') {
+        self->map = new MapImpl<int, PyObject*>();
     }
     return 0;
 }
@@ -231,8 +236,12 @@ PyObject* IIMap_getitem(pyhash_IIMapObject *self, PyObject *key) {
 }
 
 int IIMap_setitem(pyhash_IIMapObject *self, PyObject *key, PyObject *val) {
-    self->map->put(key, val);
-    return 0;
+    if (self->map->put(key, val)) {
+        return 0;
+    } else {
+        PyErr_SetObject(PyExc_KeyError, key);
+        return -1;
+    }
 }
 
 static PyObject *
@@ -251,10 +260,10 @@ IIMap_iter(pyhash_IIMapObject *self);
 //     return val;
 // }
 
-static PyMappingMethods IIMapMappingMethods {
-    .mp_length = (lenfunc) IIMap_size,
-    .mp_subscript = (binaryfunc) IIMap_getitem,
-    .mp_ass_subscript = (objobjargproc) IIMap_setitem,
+static PyMappingMethods IIMapMappingMethods = {
+    (lenfunc) IIMap_size,
+    (binaryfunc) IIMap_getitem,
+    (objobjargproc) IIMap_setitem,
 };
 
 static struct PyMethodDef IIMap_Methods[] = {
@@ -355,19 +364,19 @@ static PyTypeObject pyhash_IIMapIteratorType = {
     0,                         /* tp_clear */
     0,                         /* tp_richcompare */
     0,                         /* tp_weaklistoffset */
-    0,  /* tp_iter */
+    0,                         /* tp_iter */
     (iternextfunc) IIMapIterator_iternext, /* tp_iternext */
-    0,             /* tp_methods */
-    0,             /* tp_members */
+    0,                         /* tp_methods */
+    0,                         /* tp_members */
     0,                         /* tp_getset */
     0,                         /* tp_base */
     0,                         /* tp_dict */
     0,                         /* tp_descr_get */
     0,                         /* tp_descr_set */
     0,                         /* tp_dictoffset */
-    0,     /* tp_init */
+    0,                         /* tp_init */
     0,                         /* tp_alloc */
-    0,                 /* tp_new */
+    0,                         /* tp_new */
 };
 
 static PyObject *
@@ -382,14 +391,13 @@ IIMap_iter(pyhash_IIMapObject *self) {
 
 
 static PyMethodDef PyHashMethods[] = {
-    {"greet", greet_name, METH_VARARGS, "Greet an entity."},
     {NULL, NULL, 0, NULL}
 };
 
 static struct PyModuleDef greet =
 {
     PyModuleDef_HEAD_INIT,
-    "pyhash",     /* name of module */
+    "pyhash_ext",     /* name of module */
     "",          /* module documentation, may be NULL */
     -1,          /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
     PyHashMethods
